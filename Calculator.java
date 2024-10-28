6c2319099e3af9387d8cf09fb4e4d4d32f55db66
gena03111975 <gepota1@tut.by>
2024-10-28 20:43:04 +0300

    добавили Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    private ComplexOperation operation;
+
+    public Calculator(ComplexOperation operation) {
+        this.operation = operation;
+    }
+
+    public ComplexNumber calculate(ComplexNumber a, ComplexNumber b) {
+        return operation.operate(a, b);
+    }
+}
